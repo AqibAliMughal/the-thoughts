389a7e76c79b4dc23dd0fbcdd2bd4327192cb9eb
Aqib Ali <ahadaqib349@gmail.com>
2023-06-07 10:15:15 +0500

    Div removed from name

--- Classes/HTML.php
+++ Classes/HTML.php
@@ -29,7 +29,9 @@ class HTML
 		<body>
 		  <nav class="navbar navbar-expand-lg ">
 		    <div class="container-fluid">
-		      <a class="navbar-brand nav-text" href="javascript:void(0)">The Thoughts/div></a>
+		      <a class="navbar-brand nav-text" href="javascript:void(0)">
+						<strong>{ The Thoughts }</strong>
+					</a>
 		      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		        <span class="navbar-toggler-icon"></span>
 		      </button>
